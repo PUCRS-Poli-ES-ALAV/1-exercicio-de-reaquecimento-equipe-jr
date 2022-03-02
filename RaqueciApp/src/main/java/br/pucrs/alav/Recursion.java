b8c153a30c6fbab46e3aca0dd37be1ca9ca9eb71
Jonatasvangroll <jonatas.vangroll@gmail.com>
2022-03-02 20:44:59 -0300

    Update Recursion.java

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -1,5 +1,36 @@
 package br.pucrs.alav;
 
 public class Recursion {
+    public static void main(string args) {
+     	System.out.println(multiNaturais(6,4));
+      
+    }
+
+    public int multiNaturais(int value; int repeat){
+
+        if (repeat==0){
+            return;
+        }
+        return value+multiNaturais(value,repeat-1);
+    }
+    /*
+    public int somaNaturais(){}
+
+    public int somaMeio(){}
+
+    public int inverteString(){}
+    
+    public int geraSequencia(){}
+
+    public int sequenciaAckerman(){}
+
+    public int somaProdutoVetor(){}
+
+    public boolean palindromo(){}
+
+    public List<string> combinaPalavra(){}
+     
+    public List<int> fibonati(){}
+    */
 	
 }
