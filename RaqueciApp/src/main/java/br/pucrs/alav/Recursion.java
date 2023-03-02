927d2d624153da10866186a8094f2c716fe00648
Jonatasvangroll <jonatas.vangroll@gmail.com>
2023-03-02 18:39:23 -0300

    exercício 1 terminado

--- RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
+++ RaqueciApp/src/main/java/br/pucrs/alav/Recursion.java
@@ -2,4 +2,17 @@ package br.pucrs.alav;
 
 public class Recursion {
 	
+	public int recur (int a1, int a2){
+        if(a1<0){
+            throw new IllegalAccessException("Numero negativo")}
+
+        else if(a1==0){
+            
+            return 0;}
+
+        if (a1 >= 1) {
+        return a2;
+    }
+        return recur(a1 - 1, a2)+ a2;
+  }
 }
